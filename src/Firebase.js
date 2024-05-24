cbc1ac5d713e999940072b1654daa9299442ebae
rusuot <156461904+rusuot@users.noreply.github.com>
2024-05-24 12:00:18 +0100

    Add files via upload

---
+++ src/Firebase.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC8alsXRh40vuc14hO6cPd7fhA7MepjQjk",
+  authDomain: "nexus-ecommerce-d1cc9.firebaseapp.com",
+  projectId: "nexus-ecommerce-d1cc9",
+  storageBucket: "nexus-ecommerce-d1cc9.appspot.com",
+  messagingSenderId: "1096568496192",
+  appId: "1:1096568496192:web:091368cda00dec79783fa5",
+};
+
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+
